de1bfbf6b7510a4609ba13fa6d8877d6b303aca6
dje82 <dje82@msn.com>
2020-10-29 19:16:44 -0500

    end

--- script.js
+++ script.js
@@ -6,32 +6,24 @@ gamePad.style.height = "500px";
 //init with a 16*16 grids
 makeGrid(16);
 
-const cells = document.getElementsByClassName("grid-cell");
-
-//EVENTS LISTENERS :
-    
-    //buttons listeners
-    const btnResetGrid = document.getElementById("reset-grid");
-    btnResetGrid.addEventListener("click", resetGrid);
-
-    const btnResetColor = document.getElementById("reset-color");
-    btnResetColor.addEventListener("click", resetToBlack);
-
-    //switch Listeners
-    const switchConfetti = document.getElementById("confetti-checkbox");
-    const switchGrey = document.getElementById("grey-checkbox");
-    
-    //Managed switch between us
-    // switchConfetti.addEventListener("click", switchManaged);
-    // switchGrey.addEventListener("click", switchManaged);
-    
-    /// reste la gestion des switchs (un seul actif à la fois)
-    // gestion de la color greyToBlack
-   
-   
-
 // modif color on gamePad cells
 gamePad.addEventListener("mouseover", changeColor)
+
+
+
+//EVENTS LISTENERS :
+
+//buttons listeners
+const btnResetGrid = document.getElementById("reset-grid");
+btnResetGrid.addEventListener("click", resetGrid);
+
+const btnResetColor = document.getElementById("reset-color");
+btnResetColor.addEventListener("click", resetToBlack);
+
+//switch Listeners
+const switchConfetti = document.getElementById("confetti-checkbox");
+const switchGrey = document.getElementById("shadow-checkbox");
+
 
 
 //functions
@@ -44,59 +36,67 @@ function makeGrid(rows) {
         // Add the new div element to the gridConainer element and give it a class
         gamePad.appendChild(newDiv).className = "grid-cell";
         gamePad.appendChild(newDiv).id = `cell-${i}`;
-        
+        //set opacity to 1 (used for )
+        gamePad.appendChild(newDiv).style.opacity = 1;
+
         // Building the grid
         gamePad.style.gridTemplateRows = `repeat(${rows}, minmax(0, 1fr))`;
         gamePad.style.gridTemplateColumns = `repeat(${rows}, minmax(0, 1fr))`;
+
     }
 }
 
-function resetGrid(){   
+function resetGrid() {
     //user prompt
-    let value 
-    while (value = prompt("Please enter how many squares by side you want", 16)){
+    let value
+    while (value = prompt("Please enter how many squares by side you want", 16)) {
         if (isNaN(value)) {
             alert('Please give us a number')
-            console.log('if: '+value)
+            console.log('if: ' + value)
         } else if (value > 150 || value <= 0) {
             alert('Please the number must be between 0 and 150')
         } else {
-             //clean and call grid creation function
-             gamePad.innerHTML =''
-             gamePad.style=""
-             gamePad.style.width = "500px"
-             gamePad.style.height = "500px"
-             makeGrid(value)
-             
-             //forced reset off switchs
-             switchConfetti.checked=false
-             switchGrey.checked=false
+            //clean and call grid creation function
+            gamePad.innerHTML = ''
+            gamePad.style = ""
+            gamePad.style.width = "500px"
+            gamePad.style.height = "500px"
+            makeGrid(value)
 
-             return
+            //forced reset off switchs
+            switchConfetti.checked = false
+            switchGrey.checked = false
+
+            return
         }
     }
 }
 
-
-function changeColor(e){
+function changeColor(e) {
     let color = selectColor()
-    if(e.target.className == "container") {
+    if (e.target.className == "container") {
         return
     }
-        e.target.style.background = color
+    if (switchGrey.checked === true) {
+        console.log(e.target.style.opacity)
+        e.target.style.opacity = e.target.style.opacity - 0.2;
+    }
+    e.target.style.background = color;
+    if (switchGrey.checked === false) {
+        e.target.style.opacity = 1;
+    }
+
 }
 
-function selectColor(){
-    if(switchConfetti.checked === true) {
+function selectColor() {
+    if (switchConfetti.checked === true) {
         return randomColor()
-    } else if(switchGrey.checked === true){
-        return greyToBlack()   
     } else {
         return colorPicker = document.querySelector("#color-picker").value
-    }   
+    }
 }
 
-function randomColor(){
+function randomColor() {
     let color = '#'
     let charAllows = 'ABCDEF0123456789'
 
@@ -106,10 +106,13 @@ function randomColor(){
     return color
 }
 
-function greyToBlack(){
-    alert('coucou c grey !')
+function decreaseOpacity(value) {
+    console.log('function decrese opacity')
+    let opacity = value;
+    opacity = opacity - 0.2;
+    return opacity
 }
 
 function resetToBlack() {
-    return colorPicker = document.querySelector("#color-picker").value="#000000"
+    return colorPicker = document.querySelector("#color-picker").value = "#000000"
 }
